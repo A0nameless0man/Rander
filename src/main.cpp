65f9fa7b9fc74b0f3ddcd67af170c8828064869a
huguang.nemo <huguang.nemo@bytedance.com>
2021-06-12 01:35:12 +0800

    Template

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
-#include<iostream>
+#include <iostream>
+#include "matrix.hpp"
 
-int main(void){
-    std::cout<<"Hello"<<std::endl;
+int main(void)
+{
+    std::cout << "Hello" << std::endl;
+    Matrix::NumArray<4, 4, int> a = {{{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}}};
+    a=a/2;
+    a.print(std::cout);
+    Matrix::Matrix<4, 4, int> m = {{{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}}};
+    m=m/2;
+    m.print(std::cout);
+    // BaseMatrix::NumView<4, 4, int> &v = m;
 }
--- src/matrix.hpp
+++ src/matrix.hpp
@@ -1,6 +1,11 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
 #include <cstddef>
 #include <iostream>
 #include <array>
+#include <type_traits>
+
 namespace Matrix
 {
 
@@ -9,8 +14,8 @@ namespace Matrix
     {
     public:
         virtual V get(size_t y, size_t x) const = 0;
-        virtual V set(size_t y, size_t x, V &v) = 0;
-        virtual V set(size_t y, size_t x, V &&v) = 0;
+        virtual NumView &set(size_t y, size_t x, V &v) = 0;
+        virtual NumView &set(size_t y, size_t x, V &&v) = 0;
         template <size_t VH, size_t VW>
         class View
         {
@@ -20,36 +25,127 @@ namespace Matrix
     };
 
     template <size_t H, size_t W, typename V = double>
-    class NumArray : NumView<H, W, V>
+    class NumArray : public NumView<H, W, V>
     {
-    protected:
-        V val[H][W];
-
     public:
+        V val[H][W];
+        NumArray() {}
         NumArray(const std::array<V, H * W> &);
-        template <size_t VH, size_t VW>
-        class ArrayView : NumView<H, W, V>
+        virtual V get(size_t y, size_t x) const;
+        virtual NumArray &set(size_t y, size_t x, V &v);
+        virtual NumArray &set(size_t y, size_t x, V &&v);
+        NumArray operator/(const V &rop)
         {
-        protected:
+            NumArray res;
+#pragma omp parallel for
+            for (size_t i = 0; i < H; i++)
+            {
+                for (size_t j = 0; j < W; j++)
+                {
+                    res.set(i, j, this->get(i, j) / rop);
+                }
+            }
+            return res;
+        }
+        template <size_t VH, size_t VW>
+        class ArrayView : public NumView<H, W, V>
+        {
+        public:
             size_t offsetW;
             size_t offsetH;
         };
     };
 
-    template <size_t H, size_t W, typename V = double>
-    class Matrix : NumArray<H, W, V>
+    template <size_t H, size_t W, typename V, typename SELF>
+    class BaseMatrix : public NumArray<H, W, V>
     {
-    private:
     public:
-        Matrix();
-        ~Matrix();
+        BaseMatrix() {}
+        BaseMatrix(const std::array<V, H * W> &arr) : NumArray<H, W, V>(arr) {}
+        SELF operator/(const V &rop)
+        {
+            SELF res;
+#pragma omp parallel for
+            for (size_t i = 0; i < H; i++)
+            {
+                for (size_t j = 0; j < W; j++)
+                {
+                    res.set(i, j, this->get(i, j) / rop);
+                }
+            }
+            return res;
+        }
+    };
+    template <size_t H, size_t W, typename V = double>
+    class Matrix : public BaseMatrix<H, W, V, Matrix<H, W, V>>
+    {
+    public:
+        Matrix() {}
+        Matrix(const std::array<V, H * W> &arr) : BaseMatrix<H, W, V, Matrix<H, W, V>>(arr) {}
     };
 
     template <size_t W, typename V>
-    class Matrix<W, W, V>
+    class Matrix<W, W, V> : public BaseMatrix<W, W, V, Matrix<W, W, V>>
     {
     public:
+        Matrix() {}
+        Matrix(const std::array<V, W * W> &arr) : BaseMatrix<W, W, V, Matrix<W, W, V>>(arr) {}
         Matrix<W, W, V> inv() const;
     };
 
+    template <size_t H, size_t W, typename V>
+    size_t NumView<H, W, V>::print(std::ostream &os) const
+    {
+        for (size_t i = 0; i < H; i++)
+        {
+            for (size_t j = 0; j < W; j++)
+            {
+                os << this->get(i, j) << " ";
+            }
+            os << "\n";
+        }
+        return H * W;
+    }
+
+    template <size_t H, size_t W, typename V>
+    NumArray<H, W, V>::NumArray(const std::array<V, H * W> &arr)
+    {
+        auto it = arr.cbegin();
+        for (size_t i = 0; i < H; i++)
+        {
+            for (size_t j = 0; j < W; j++)
+            {
+                val[i][j] = *it;
+                ++it;
+            }
+        }
+    }
+
+    template <size_t H, size_t W, typename V>
+    V NumArray<H, W, V>::get(size_t y, size_t x) const
+    {
+        return val[y][x];
+    }
+
+    template <size_t H, size_t W, typename V>
+    NumArray<H, W, V> &NumArray<H, W, V>::set(size_t y, size_t x, V &v)
+    {
+        val[y][x] = v;
+        return *this;
+    }
+
+    template <size_t H, size_t W, typename V>
+    NumArray<H, W, V> &NumArray<H, W, V>::set(size_t y, size_t x, V &&v)
+    {
+        val[y][x] = v;
+        return *this;
+    }
+
+    template <size_t W, typename V>
+    Matrix<W, W, V> Matrix<W, W, V>::inv() const
+    {
+        return this;
+    }
 }
+
+#endif
